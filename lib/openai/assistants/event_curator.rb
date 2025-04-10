42b5a18c9dbb2d309f3c366a978ee650e3166992
lazyorcas <thelazyorcas@gmail.com>
2025-04-10 16:01:34 +0800

    fix

--- lib/openai/assistants/event_curator.rb
+++ lib/openai/assistants/event_curator.rb
@@ -1,36 +1,44 @@
 class OpenAI::Assistants::EventCurator
   INSTRUCTIONS = "You are a helpful event curator that builds search queries for a search engine."
   INPUT_TEMPLATE = <<-TEXT
-    Build a search query from the following user input.
+    Build a search query from the following user input and rules below.
 
-    ## User Context
-    The current date is %{current_date}.
-    The current day of the week is %{current_dow}.
-    The current time is %{current_time}.
-    The current year is %{current_year}.
+    ## User Input
+    %{text}
 
-    ## Facts
+    ## Keywords Rules
+    - The keywords should exclude information related to date, time, price (including "free").
+    - The keywords should exclude determiner words like "every", "all".
+    - The keywords should exclude adjectives.
+
+    ## Date
+    If a time-sensitive keyword is mentioned, please use the section below to build the date range.
+
+    ### User's Context
+    - The current date is %{current_date}.
+    - The current day of the week is %{current_dow}.
+    - The current time is %{current_time}.
+    - The current year is %{current_year}.
+
+    ### Facts
     - Monday is the first day of the week with index 0.
     - Sunday is the last day of the week with index 6.
     - A week has 7 days.
 
-    ## Date and Time Rules
-    If/when a relative date is mentioned, please use these rules. It's safe to consider variations of these rules.
+    ### Rules
+    It's safe to consider variations of these rules.
     - "today", "tomorrow", "yesterday" should have a date time range that covers the entire day.
     - "this week", "next week" should have a date time range that covers the entire week.
     - "this month", "next month" should have a date time range that covers the entire month.
     - "this year", "next year" should have a date time range that covers the entire year.
     - Start time should be 00:00:00 and end time should be 23:59:59.
+
+    ### Notes
     - It's possible to have a start date before the current date.
 
-    ## Rules
-    - The keywords should exclude information related to date, time, price (including "free").
-    - The keywords should exclude determiner words like "every", "all".
-    - The keywords should exclude adjectives.
-    - Your answer must not include that are not mentioned in the user input.
-
-    ## User Input
-    %{text}
+    ## Hard Rules
+    - You must not include any information that is not mentioned in the user input.
+    - You must not assume anything.
   TEXT
 
   def initialize
