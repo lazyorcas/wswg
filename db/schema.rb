c42d555dce49374c0559ce44aaffdb62310eece3
lazyorcas <thelazyorcas@gmail.com>
2025-04-23 13:42:39 +0800

    remove type from events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_23_045934) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_23_053605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -114,7 +114,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_23_045934) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.string "type"
     t.bigint "city_id", null: false
     t.bigint "source_id", null: false
     t.string "uid", null: false
