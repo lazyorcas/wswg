055ccb946b8f384ccbef7493b3a00add8ccd7b5f
lazyorcas <thelazyorcas@gmail.com>
2025-04-23 22:42:38 +0800

    add non-null check to searchable model types in searches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_23_141020) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_23_144213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -173,7 +173,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_23_141020) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "search_query_id", null: false
-    t.string "searchable_model_type"
+    t.string "searchable_model_type", null: false
     t.index ["search_query_id"], name: "index_searches_on_search_query_id"
   end
 
