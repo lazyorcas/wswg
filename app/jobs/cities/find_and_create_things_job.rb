3031963a5cc6f9bbe05327adfc01309a58951a9f
lazyorcas <thelazyorcas@gmail.com>
2025-04-24 22:48:15 +0800

    only scrape with proxy once per day

--- app/jobs/cities/find_and_create_things_job.rb
+++ app/jobs/cities/find_and_create_things_job.rb
@@ -2,14 +2,18 @@ class Cities::FindAndCreateThingsJob < ApplicationJob
   queue_with_priority 2
 
   HOURS_TO_FIND = [ 7, 12, 17, 22 ]
+  PROXY_HOUR_TO_FIND = 22
 
   def perform
-    City.find_each do |city|
+    City.includes(:sources).find_each do |city|
       current_hour = Time.current.in_time_zone(city.time_zone).hour
 
       if HOURS_TO_FIND.include?(current_hour)
-        city.source_ids.each do |source_id|
-          Source::FindAndCreateThingsJob.perform_later(source_id)
+        city.sources.each do |source|
+          # proxy is expensive, so we only use it once a day
+          next if source.proxy? && current_hour != PROXY_HOUR_TO_FIND
+
+          Source::FindAndCreateThingsJob.perform_later(source.id)
         end
       end
     end
