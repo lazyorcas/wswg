86960cbfe4509fd13f0b1cb4b7b3e20ebb93183f
lazyorcas <thelazyorcas@gmail.com>
2025-04-25 11:47:36 +0800

    handle open ai server error

--- app/jobs/event/create_job.rb
+++ app/jobs/event/create_job.rb
@@ -6,6 +6,7 @@ class Event::CreateJob < ApplicationJob
 
   # when OpenAI fails
   retry_on Faraday::TooManyRequestsError, wait: 5.minutes, attempts: 3
+  retry_on Faraday::ServerError, wait: 15.minutes, attempts: 2
 
   rescue_from(Event::NotFoundViaUrlError) do |exception|
     DeadLink.find_or_create_by!(url: exception.url)
