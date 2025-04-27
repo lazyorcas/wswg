bd175f4341b7c3ae9edeb546856087343b6ec63e
lazyorcas <thelazyorcas@gmail.com>
2025-04-27 16:56:49 +0800

    chore

--- app/models/event.rb
+++ app/models/event.rb
@@ -23,9 +23,12 @@ class Event < ApplicationRecord
   validates :start_time, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
-  validate :validate_url_found, on: :create
-  validate :validate_end_date_is_today_or_future, on: :create
-  validate :validate_not_duplicated, on: :create, if: -> { location_id.present? && start_date.present? && end_date.present? && start_time.present? }
+  with_options on: :create do
+    validate :validate_url_found
+    validate :validate_end_date_is_today_or_future
+    validate :validate_not_duplicated, if: -> { location_id.present? && start_date.present? && end_date.present? && start_time.present? }
+  end
+
   validate :validate_start_date_is_before_or_same_as_end_date
 
   before_update -> { self.location = nil }, if: :location_query_changed?
