b40ea7523cac1ff5542602b613cfd740f4bff641
lazyorcas <thelazyorcas@gmail.com>
2025-04-29 10:34:26 +0800

    fix

---
+++ db/migrate/20250429023407_remove_alias_from_city.rb
@@ -0,0 +1,5 @@
+class RemoveAliasFromCity < ActiveRecord::Migration[8.0]
+  def change
+    remove_column :cities, :alias
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_28_075614) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_29_023407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -100,7 +100,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_28_075614) do
     t.float "longitude", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "alias"
     t.bigint "time_zone_id", null: false
     t.index ["slug"], name: "index_cities_on_slug", unique: true
     t.index ["time_zone_id"], name: "index_cities_on_time_zone_id"
