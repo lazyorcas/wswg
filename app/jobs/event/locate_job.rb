c5190d13ccec8373adf6394f195b079fbc7f5b10
lazyorcas <thelazyorcas@gmail.com>
2025-04-30 13:48:43 +0800

    fix

--- app/jobs/event/locate_job.rb
+++ app/jobs/event/locate_job.rb
@@ -1,4 +1,6 @@
 class Event::LocateJob < ApplicationJob
+  MAX_DISTANCE_IN_KM = 100
+
   queue_with_priority 3
 
   def perform(id, location_query:)
@@ -6,6 +8,16 @@ class Event::LocateJob < ApplicationJob
     return if location_query.location_id.blank?
 
     event = Event.find(id)
+
+    coordinates = location_query.location.coordinates
+    city_coordinates = event.city_source.city.coordinates
+
+    distance_from_city_center = Geospatial.distance_in_km_between(coordinates, city_coordinates)
+
+    if distance_from_city_center > MAX_DISTANCE_IN_KM
+      raise StandardError.new("Event is too far away")
+    end
+
     event.location_id = location_query.location_id
     event.save!
   end
