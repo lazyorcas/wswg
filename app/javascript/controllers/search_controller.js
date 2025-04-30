b4afbb46b3cefe128e3c0e04339e9e8038492a1a
lazyorcas <thelazyorcas@gmail.com>
2025-04-30 17:58:19 +0800

    chore

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,16 +2,16 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["searchInput", "results"];
-
-  connect() {
-    this.searchInputTarget.addEventListener("input", this.search.bind(this));
-  }
   
   clear() {
     this.resultsTarget.innerHTML = "";
   }
 
-  search(e) {
+  blur() {
+    this.searchInputTarget.blur();
+  }
+
+  input(e) {
     if (e.target.value === "") {
       this.clear();
     }
