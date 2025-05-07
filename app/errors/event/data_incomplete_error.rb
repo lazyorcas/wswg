874171c42d7da143d7defb69e0ee200d66434ade
lazyorcas <thelazyorcas@gmail.com>
2025-05-07 14:01:26 +0800

    fix

--- app/errors/event/data_incomplete_error.rb
+++ app/errors/event/data_incomplete_error.rb
@@ -1,6 +1,5 @@
 class Event::DataIncompleteError < StandardError
   def initialize(event)
-    errors = event.errors.reject { |error| error.type == :data_incomplete }
-    super(errors.full_messages.join(", "))
+    super(event.errors.full_messages.join(", "))
   end
 end
