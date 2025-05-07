cc4c980442d7c0d6d04209643e7b7015ec241173
lazyorcas <thelazyorcas@gmail.com>
2025-05-07 14:34:37 +0800

    fix

--- app/models/concerns/city/scrapeable.rb
+++ app/models/concerns/city/scrapeable.rb
@@ -2,8 +2,10 @@ module City::Scrapeable
   extend ActiveSupport::Concern
 
   def scrape(limit)
+    limit_per_city_source = limit / city_sources.count
+
     city_sources.each do |city_source|
-      city_source.queue_scrape(limit)
+      city_source.queue_scrape(limit_per_city_source)
     end
   end
 end
