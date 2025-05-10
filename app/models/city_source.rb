d89bb0ae078563ac3fbe9d4b4f75386588fcbfdd
lazyorcas <thelazyorcas@gmail.com>
2025-05-10 07:00:59 +0200

    fix

--- app/models/city_source.rb
+++ app/models/city_source.rb
@@ -6,6 +6,8 @@ class CitySource < ApplicationRecord
   belongs_to :city
   belongs_to :source
 
+  has_many :events
+
   def self.find_by_names(city_name:, source_name:)
     city = City.find_by(name: city_name)
     source = Source.find_by(name: source_name)
