aaa98232664c90d71a368aa90b7f2c1f2741a9c4
lazyorcas <thelazyorcas@gmail.com>
2025-05-10 07:19:08 +0200

    fix

--- app/models/city_source.rb
+++ app/models/city_source.rb
@@ -1,4 +1,5 @@
 class CitySource < ApplicationRecord
+  include Availability
   include Scrapeable
 
   scope :enabled, -> { where(enabled: true) }
@@ -12,38 +13,6 @@ class CitySource < ApplicationRecord
     city = City.find_by(name: city_name)
     source = Source.find_by(name: source_name)
     find_by(city: city, source: source)
-  end
-
-  def self.enable(city_names: nil, source_names: nil)
-    city_ids = city_names.present? ?
-      City.where(name: city_names).pluck(:id) :
-      City.pluck(:id)
-
-    source_ids = source_names.present? ?
-      Source.where(name: source_names).pluck(:id) :
-      Source.pluck(:id)
-
-    city_ids.each do |city_id|
-      source_ids.each do |source_id|
-        find_by(city_id: city_id, source_id: source_id).update!(enabled: true)
-      end
-    end
-  end
-
-  def self.disable(city_names: nil, source_names: nil)
-    city_ids = city_names.present? ?
-      City.where(name: city_names).pluck(:id) :
-      City.pluck(:id)
-
-    source_ids = source_names.present? ?
-      Source.where(name: source_names).pluck(:id) :
-      Source.pluck(:id)
-
-    city_ids.each do |city_id|
-      source_ids.each do |source_id|
-        find_by(city_id: city_id, source_id: source_id).update!(enabled: false)
-      end
-    end
   end
 
   def url
---
+++ app/models/concerns/city_source/availability.rb
@@ -0,0 +1,51 @@
+module CitySource::Availability
+  extend ActiveSupport::Concern
+
+  included do
+    scope :enabled, -> { where(enabled: true) }
+  end
+
+  def self.enable(city_names: nil, source_names: nil)
+    city_ids = find_city_ids_by_city_names(city_names)
+    source_ids = find_source_ids_by_source_names(source_names)
+
+    city_ids.each do |city_id|
+      source_ids.each do |source_id|
+        find_by(city_id: city_id, source_id: source_id).update!(enabled: true)
+      end
+    end
+  end
+
+  def self.disable(city_names: nil, source_names: nil)
+    city_ids = find_city_ids_by_city_names(city_names)
+    source_ids = find_source_ids_by_source_names(source_names)
+
+    city_ids.each do |city_id|
+      source_ids.each do |source_id|
+        find_by(city_id: city_id, source_id: source_id).update!(enabled: false)
+      end
+    end
+  end
+
+  private
+
+  def self.find_city_ids_by_city_names(city_names, all_if_nil: true)
+    if city_names.present?
+      City.where(name: city_names).pluck(:id)
+    elsif all_if_nil
+      City.pluck(:id)
+    else
+      []
+    end
+  end
+
+  def self.find_source_ids_by_source_names(source_names, all_if_nil: true)
+    if source_names.present?
+      Source.where(name: source_names).pluck(:id)
+    elsif all_if_nil
+      Source.pluck(:id)
+    else
+      []
+    end
+  end
+end
