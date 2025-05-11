9d0fc392382394afee423d73fa5ee04d08fbe4d7
lazyorcas <thelazyorcas@gmail.com>
2025-05-11 10:36:39 +0200

    fix

--- app/jobs/city_source/scrape_and_create_events_job.rb
+++ app/jobs/city_source/scrape_and_create_events_job.rb
@@ -1,14 +1,16 @@
 class NoEventsFoundError < StandardError; end
 
 class CitySource::ScrapeAndCreateEventsJob < ApplicationJob
+  NO_EVENTS_FOUND_MAX_ATTEMPTS = 3
+
   queue_with_priority 2
 
   retry_on Ferrum::TimeoutError, wait: 30.minutes, attempts: 3
   retry_on Ferrum::NodeNotFoundError, wait: 1.minute, attempts: 3
-  retry_on NoEventsFoundError, wait: 5.minutes, attempts: 3
+  retry_on NoEventsFoundError, wait: 5.minutes, attempts: NO_EVENTS_FOUND_MAX_ATTEMPTS
 
   rescue_from NoEventsFoundError do |exception|
-    if exception_executions[NoEventsFoundError.to_s] >= 3
+    if (exception_executions[NoEventsFoundError.to_s] || 0) >= NO_EVENTS_FOUND_MAX_ATTEMPTS
       return
     end
     raise exception
