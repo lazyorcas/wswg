5c5c67e21cecfc5c3c3c42b34c2eda75dde1ed6e
lazyorcas <thelazyorcas@gmail.com>
2025-05-11 10:34:07 +0200

    fix

--- app/jobs/city_source/scrape_and_create_events_job.rb
+++ app/jobs/city_source/scrape_and_create_events_job.rb
@@ -5,15 +5,20 @@ class CitySource::ScrapeAndCreateEventsJob < ApplicationJob
 
   retry_on Ferrum::TimeoutError, wait: 30.minutes, attempts: 3
   retry_on Ferrum::NodeNotFoundError, wait: 1.minute, attempts: 3
-  retry_on NoEventsFoundError, wait: 5.minutes, attempts: 5
+  retry_on NoEventsFoundError, wait: 5.minutes, attempts: 3
+
+  rescue_from NoEventsFoundError do |exception|
+    if exception_executions[NoEventsFoundError.to_s] >= 3
+      return
+    end
+    raise exception
+  end
 
   def perform(id, limit:)
     city_source = CitySource.find(id)
 
     events_attributes = city_source.scrape
-    if events_attributes.empty?
-      raise NoEventsFoundError
-    end
+    raise NoEventsFoundError if events_attributes.empty?
 
     events_attributes = events_attributes.take(limit)
 
