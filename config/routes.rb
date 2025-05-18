0b5dd7bd4512f6f0830a5f1f4c639586ec4997eb
lazyorcas <thelazyorcas@gmail.com>
2025-05-18 15:21:57 +0200

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -24,19 +24,19 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   get "/auth/:provider/callback", to: "sessions#create"
 
-  get "user/no_credits", to: "user#no_credits", as: :user_no_credits
-  get "user/top_up_credits", to: "user#top_up_credits", as: :user_top_up_credits
+  get "/user/no_credits", to: "user#no_credits", as: :user_no_credits
+  get "/user/top_up_credits", to: "user#top_up_credits", as: :user_top_up_credits
 
   get "/map", to: "map#index", as: :map
   namespace :map do
     resources :events, only: [ :index, :show ]
     resources :search_queries, only: [ :index, :create, :show ]
 
-    get "bookmarks/events", to: "bookmarks/events#index", as: :bookmarked_events
+    get "/bookmarks/events", to: "bookmarks/events#index", as: :bookmarked_events
   end
 
   resources :seens, only: [ :create ]
   resources :bookmarks, only: [ :create, :update ]
 
-  post "stripe/webhook", to: "stripe#webhook"
+  post "/stripe/webhook", to: "stripe#webhook"
 end
