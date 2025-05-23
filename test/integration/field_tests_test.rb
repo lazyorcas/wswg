8b92414427750ba3b7d5de7d0e8df686a19b2088
lazyorcas <thelazyorcas@gmail.com>
2025-05-23 14:22:41 +0800

    add sign up page test

--- test/integration/field_tests_test.rb
+++ test/integration/field_tests_test.rb
@@ -3,19 +3,53 @@ require "test_helper"
 class FieldTestsTest < ActionDispatch::IntegrationTest
   USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 
-  test "should show pricing page" do
-    get root_path, headers: { "User-Agent" => USER_AGENT }
-    FieldTest::Membership.first.update(variant: "t")
+  test "should a/b test pricing page" do
+    @path = root_path
+    @experiment = "pricing_page_shown"
 
-    get root_path, headers: { "User-Agent" => USER_AGENT }
+    init
+    change_variant_to("t")
     assert_select "a[href=?]", pricing_path
+
+    change_variant_to("f")
+    assert_select "a[href=?]", pricing_path, count: 0
   end
 
-  test "should not show pricing page" do
-    get root_path, headers: { "User-Agent" => USER_AGENT }
-    FieldTest::Membership.first.update(variant: "f")
+  test "should a/b test sign up page" do
+    @path = new_user_path
+    @experiment = "sign_up_page"
 
-    get root_path, headers: { "User-Agent" => USER_AGENT }
-    assert_select "a[href=?]", pricing_path, count: 0
+    init
+    change_variant_to("control")
+    assert_select "#hero-image-container", count: 0
+    assert_select "h1", text: "Create an account"
+
+    change_variant_to("has_free_word")
+    assert_select "#hero-image-container", count: 0
+    assert_select "h1", text: "Create a free account"
+
+    change_variant_to("has_hero_image")
+    assert_select "#hero-image-container"
+    assert_select "h1", text: "Create an account"
+
+    change_variant_to("has_free_word_and_hero_image")
+    assert_select "#hero-image-container"
+    assert_select "h1", text: "Create a free account"
+  end
+
+  private
+
+  def init
+    visit
+    @membership = FieldTest::Membership.find_by(experiment: @experiment)
+  end
+
+  def change_variant_to(variant)
+    @membership.update(variant: variant)
+    visit
+  end
+
+  def visit
+    get @path, headers: { "User-Agent" => USER_AGENT }
   end
 end
