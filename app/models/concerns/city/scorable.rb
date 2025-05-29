8b5c05d4c1f25d30e5ab3d64f22f2c3a47f24025
lazyorcas <thelazyorcas@gmail.com>
2025-05-29 14:38:03 +0800

    fix N+1

--- app/models/concerns/city/scorable.rb
+++ app/models/concerns/city/scorable.rb
@@ -6,26 +6,26 @@ module City::Scorable
   POPULAR_CITY_MIN_VISITOR_COUNT = 250
 
   def current_score
-    active_user_score + visitor_score
+    @current_score ||= active_user_score + visitor_score
   end
 
   def active_user_score
-    (1.0 * active_user_count / POPULAR_CITY_MIN_USER_COUNT).round(2)
+    @active_user_score ||= (1.0 * active_user_count / POPULAR_CITY_MIN_USER_COUNT).round(2)
   end
 
   def visitor_score
-    (1.0 * visitor_count / POPULAR_CITY_MIN_VISITOR_COUNT).round(2)
+    @visitor_score ||= (1.0 * visitor_count / POPULAR_CITY_MIN_VISITOR_COUNT).round(2)
   end
 
   def active_user_count
-    users
+    @active_user_count ||= users
       .joins(:visits)
       .where(visits: { started_at: TIME_WINDOW.ago.. })
       .count
   end
 
   def visitor_count
-    Ahoy::Event
+    @visitor_count ||= Ahoy::Event
       .non_user
       .joins(:visit)
       .where(name: "Viewed events", time: TIME_WINDOW.ago..)
