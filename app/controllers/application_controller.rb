d1beed92e38daa2ae417daee18c3e156792b0fc6
lazyorcas <thelazyorcas@gmail.com>
2025-05-29 10:16:40 +0800

    chore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@ class ApplicationController < ActionController::Base
   include Authentication
   include Dateful
   include Flash
-
-  private
-
-  def render_device_not_supported
-    render file: "#{Rails.root}/public/404-unsupported-device.html", layout: false, status: :not_found
-  end
 end
