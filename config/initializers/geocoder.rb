d8222214216e1005d923133204f26b8de3a88abe
lazyorcas <thelazyorcas@gmail.com>
2025-05-29 14:27:02 +0800

    wip

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,12 +1,14 @@
 Geocoder.configure(
   # timeout: 3,                 # geocoding service timeout (secs)
-  lookup: :mapbox,              # name of geocoding service (symbol)
-  # ip_lookup: :mapbox,           # name of IP address geocoding service (symbol)
+  # mapbox: {
+  #   api_key: ENV["MAPBOX_ACCESS_TOKEN"]
+  # },
+  ip_lookup: :ipinfo_io,        # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: ENV["MAPBOX_ACCESS_TOKEN"],               # API key for geocoding service
+  api_key: ENV["IPINFO_ACCESS_TOKEN"],               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
 
   # Exceptions that should not be rescued by default
@@ -15,7 +17,7 @@ Geocoder.configure(
   # always_raise: [],
 
   # Calculation options
-  units: :km,                 # :km for kilometers or :mi for miles
+  # units: :km,                 # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
 
   # Cache configuration
