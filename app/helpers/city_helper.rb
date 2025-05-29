22e0894b505330cc9d84b51fa74d30a2c456468e
lazyorcas <thelazyorcas@gmail.com>
2025-05-30 00:15:52 +0800

    fix

--- app/helpers/city_helper.rb
+++ app/helpers/city_helper.rb
@@ -12,7 +12,7 @@ module CityHelper
   end
 
   def build_city_description(city, time_period)
-    description = "What's happening in #{city.name} #{time_period.to_s} #{time_period.date_range_s.present? ? "(" + time_period.date_range_s + ")" : ""}? Discover local events from Luma, Meetup, Eventbrite, and Ticketmaster." # rubocop:disable Lint/RedundantStringCoercion
+    description = "What's happening in #{city.name} #{time_period.to_s} #{build_time_range(time_period)}? Discover local events from Luma, Meetup, Eventbrite, and Ticketmaster." # rubocop:disable Lint/RedundantStringCoercion
     description.gsub!(/  +/, " ")
     description.gsub!(/ \?/, "?")
     description
@@ -23,6 +23,19 @@ module CityHelper
       all_city_events_path(**args)
     else
       city_events_path(time_period_slug: time_period_slug, **args)
+    end
+  end
+
+  def build_time_range(time_period)
+    if time_period.today? || time_period.tonight? || time_period.tomorrow?
+      start_date = time_period.start_date
+      "(<time datetime=\"#{start_date.strftime("%Y-%m-%d")}\">#{start_date.strftime("%B %d")}</time>)"
+    elsif time_period.all?
+      nil
+    else
+      start_date = time_period.start_date
+      end_date = time_period.end_date
+      "(<time datetime=\"#{start_date.strftime("%Y-%m-%d")}\">#{start_date.strftime("%B %d")}</time> - <time datetime=\"#{end_date.strftime("%Y-%m-%d")}\">#{end_date.strftime("%B %d")}</time>)"
     end
   end
 end
