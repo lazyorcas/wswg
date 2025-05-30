3bd245894c0ae4ab6f8bbf91af64a81354c26368
lazyorcas <thelazyorcas@gmail.com>
2025-05-30 11:41:24 +0800

    seo

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -19,7 +19,21 @@ SitemapGenerator::Sitemap.create do
     .order(:name)
     .each do |city|
       TimePeriod::SYMBOLS.each do |time_period_symbol|
-        add build_city_events_path(city_slug: city.slug, time_period_slug: TimePeriod.slugify(time_period_symbol)), priority: 0.9, changefreq: "daily"
+        change_freq = case time_period_symbol
+        when :today, :tonight, :tomorrow, :this_week
+          "hourly"
+        when :this_weekend, :next_week, :next_weekend
+          "daily"
+        else
+          "weekly"
+        end
+
+        add build_city_events_path(
+          city_slug: city.slug,
+          time_period_slug: TimePeriod.slugify(time_period_symbol)
+        ),
+        priority: 0.9,
+        changefreq: change_freq
       end
     end
 end
