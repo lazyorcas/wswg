c80ea1c6e1d617789c660c983f09d18f903237df
lazyorcas <thelazyorcas@gmail.com>
2025-05-30 14:35:58 +0800

    fix

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -8,6 +8,7 @@ Ahoy.api = false
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
 Ahoy.geocode = true
+Ahoy.job_queue = :low_priority
 
 Ahoy.mask_ips = true
 Ahoy.cookies = :none
