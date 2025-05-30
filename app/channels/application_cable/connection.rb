44c287fc8e905b2f9acebbd57f40e22c4b3159d5
lazyorcas <thelazyorcas@gmail.com>
2025-05-30 23:41:03 +0800

    fix: action cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,14 +1,19 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    include Passwordless::ControllerHelpers
+
+    attr_accessor :session
+
     identified_by :current_user
 
     def connect
+      self.session = request.session
       self.current_user = find_verified_user
     end
 
     private
       def find_verified_user
-        if verified_user = User.find_by(id: request.session[:user_id])
+        if verified_user = User.find_by(id: session[:user_id]) || authenticate_by_session(User)
           verified_user
         else
           reject_unauthorized_connection
