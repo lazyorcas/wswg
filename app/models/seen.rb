98b1ce129bc029c2731ba7546fb606132fe7c6e9
lazyorcas <thelazyorcas@gmail.com>
2025-05-30 23:46:37 +0800

    wip

--- app/models/seen.rb
+++ app/models/seen.rb
@@ -1,6 +1,6 @@
 class Seen < ApplicationRecord
   belongs_to :user, optional: true
-  belongs_to :event, class_name: "::Event"
+  belongs_to :event, class_name: "::Event", touch: true
 
   validates :event_id, uniqueness: { scope: :user_id }, if: -> { user.present? }
 end
