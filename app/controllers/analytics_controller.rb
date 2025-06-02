04cad8accd120b9ff42d30a19faea38ffdd66bdf
lazyorcas <thelazyorcas@gmail.com>
2025-06-02 12:33:43 +0800

    fix

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -147,13 +147,6 @@ class AnalyticsController < ApplicationController
       .where("ahoy_events.time >= ?", @start_date)
       .group("properties->'params'->>'query'")
       .count
-    @sign_up_page_bookmarks = Ahoy::Event
-      .non_user
-      .where(name: "Visited sign up page")
-      .where("properties->'params'->>'bookmark_event_id' IS NOT NULL")
-      .where("ahoy_events.time >= ?", @start_date)
-      .group_by_period(@time_interval, :time, range: @time_range, expand_range: true)
-      .count
     @sign_up_page_sources = Ahoy::Event
       .non_user
       .where(name: "Visited sign up page")
