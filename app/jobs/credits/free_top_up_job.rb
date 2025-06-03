453763032fc80ba414e147864bde447cce471510
lazyorcas <thelazyorcas@gmail.com>
2025-06-03 13:46:01 +0800

    wip

---
+++ app/jobs/credits/free_top_up_job.rb
@@ -0,0 +1,8 @@
+class Credits::FreeTopUpJob < ApplicationJob
+  queue_with_priority 4
+
+  def perform
+    User.where(credits: 0).find_each(&:add_free_credits!)
+    Visitor.where(credits: 0).find_each(&:add_free_credits!)
+  end
+end
--- app/jobs/users/credits/free_top_up_job.rb
+++
@@ -1,9 +0,0 @@
-class Users::Credits::FreeTopUpJob < ApplicationJob
-  queue_with_priority 4
-
-  def perform
-    User.where(credits: 0).find_each do |user|
-      user.add_free_credits!
-    end
-  end
-end
