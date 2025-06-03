9e6951efd380af9afb64c2f7942d17feb3d25a9a
lazyorcas <thelazyorcas@gmail.com>
2025-06-03 13:42:43 +0800

    fix

--- app/controllers/home/events_controller.rb
+++ app/controllers/home/events_controller.rb
@@ -124,5 +124,7 @@ class Home::EventsController < ApplicationController
 
     Current.person.seen_events << @event
     Current.person.save!
+  rescue => e
+    Sentry.capture_exception(e)
   end
 end
