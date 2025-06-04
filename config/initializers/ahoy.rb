9f4409c59bd33272a272865a08d58901823d3868
lazyorcas <thelazyorcas@gmail.com>
2025-06-05 01:53:37 +0800

    wip

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -18,5 +18,5 @@ Ahoy.api = false
 # set to true for geocoding (and add the geocoder gem to your Gemfile)
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
-Ahoy.geocode = true
-Ahoy.job_queue = :low_priority
+Ahoy.geocode = false
+# Ahoy.job_queue = :low_priority
