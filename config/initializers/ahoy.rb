9ba5709c62bcb52ef5047bf6e024f9b2637988e7
lazyorcas <thelazyorcas@gmail.com>
2025-06-05 19:41:51 +0800

    wip

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -26,7 +26,7 @@ class Ahoy::Store < Ahoy::DatabaseStore
       data[:ip] = Ahoy.mask_ip(request.remote_ip)
 
       if lat.present? && lon.present?
-        noisy_coords = Geospatial.add_noise_to_coords({ lat: lat.to_f, lon: lon.to_f })
+        noisy_coords = Geospatial.add_noise_to_coords({ lat: lat.to_f.round(4), lon: lon.to_f.round(4) })
         data[:latitude] = noisy_coords[:lat]
         data[:longitude] = noisy_coords[:lon]
       end
