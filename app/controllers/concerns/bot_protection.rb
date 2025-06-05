141a330c9699cdd0fcdf5fbd1a7bb410e67310c0
lazyorcas <thelazyorcas@gmail.com>
2025-06-05 21:54:44 +0800

    fix

--- app/controllers/concerns/bot_protection.rb
+++ app/controllers/concerns/bot_protection.rb
@@ -3,7 +3,7 @@ module BotProtection
 
   class_methods do
     def protect_from_bots(options = {})
-      before_action -> { head(:forbidden) }, { if: browser.bot? }.merge(options)
+      before_action -> { head(:forbidden) }, { if: -> { browser.bot? } }.merge(options)
     end
   end
 end
