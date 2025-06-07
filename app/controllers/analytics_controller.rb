23e37af2fd6be4f12c1cb9ae6514f6435adb3c90
lazyorcas <thelazyorcas@gmail.com>
2025-06-08 02:00:12 +0800

    fix

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -149,7 +149,7 @@ class AnalyticsController < AdminController
       .reverse
     @map_page_queries = Ahoy::Event
       .non_user
-      .where(name: "Searched")
+      .where(name: [ "Searched", "Searched on map page" ])
       .where("properties->>'query' IS NOT NULL")
       .where("ahoy_events.time >= ?", @start_date)
       .group("properties->>'query'")
