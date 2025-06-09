233e082531f9a5fb7465a49d7f29c50b5e9487ed
lazyorcas <thelazyorcas@gmail.com>
2025-06-09 18:02:46 +0800

    sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,4 +1,5 @@
 SitemapGenerator::Interpreter.send :include, CityHelper
+SitemapGenerator::Interpreter.send :include, NearbyHelper
 
 SitemapGenerator::Sitemap.default_host = "https://#{ENV["HOST_NAME"]}"
 SitemapGenerator::Sitemap.compress = false
@@ -16,26 +17,30 @@ SitemapGenerator::Sitemap.create do
 
   add local_events_directory_path, priority: 0.8, changefreq: "daily"
 
-  City
-    .enabled
-    .order(:name)
-    .each do |city|
-      TimePeriod::SYMBOLS.each do |time_period_symbol|
-        change_freq = case time_period_symbol
-        when :all, :today, :tonight, :tomorrow, :this_week
-          "hourly"
-        when :this_weekend, :next_week, :next_weekend
-          "daily"
-        else
-          "weekly"
-        end
-
-        add build_city_events_path(
-          city_slug: city.slug,
-          time_period_slug: TimePeriod.slugify(time_period_symbol)
-        ),
-        priority: 0.9,
-        changefreq: change_freq
-      end
+  cities = City.enabled
+  TimePeriod::SYMBOLS.each do |time_period_symbol|
+    change_freq = case time_period_symbol
+    when :all, :today, :tonight, :tomorrow, :this_week
+      "hourly"
+    when :this_weekend, :next_week, :next_weekend
+      "daily"
+    else
+      "weekly"
     end
+
+    add build_nearby_events_path(
+      time_period_slug: TimePeriod.slugify(time_period_symbol)
+    ),
+    priority: 0.9,
+    changefreq: change_freq
+
+    cities.each do |city|
+      add build_city_events_path(
+        city_slug: city.slug,
+        time_period_slug: TimePeriod.slugify(time_period_symbol)
+      ),
+      priority: 0.9,
+      changefreq: change_freq
+    end
+  end
 end
