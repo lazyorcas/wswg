efd43ee67cb4aad5952acc39da9d570bc3eac375
lazyorcas <thelazyorcas@gmail.com>
2025-06-10 22:39:29 +0800

    fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "admin", preload: false
-pin "chartkick", to: "chartkick.js", preload: false
-pin "Chart.bundle", to: "Chart.bundle.js", preload: false
+pin "admin", preload: [ "admin" ]
+pin "chartkick", to: "chartkick.js", preload: [ "admin " ]
+pin "Chart.bundle", to: "Chart.bundle.js", preload: [ "admin " ]
