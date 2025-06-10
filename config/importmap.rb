5d11cb0e9425bc966a6907ffaa261a8ef5244f1c
lazyorcas <thelazyorcas@gmail.com>
2025-06-10 15:57:32 +0800

    fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,10 +3,9 @@
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 
-pin "stimulus", preload: false
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: false
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: false
-pin_all_from "app/javascript/controllers", under: "controllers", preload: false
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "admin", preload: false
 pin "chartkick", to: "chartkick.js", preload: false
