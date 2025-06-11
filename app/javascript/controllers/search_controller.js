e749b4ed8293f4ca96819fab884f164d531bf154
lazyorcas <thelazyorcas@gmail.com>
2025-06-11 22:11:15 +0800

    fix

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["searchInput", "results", "status"];
+  static targets = ["searchInput", "submitButton", "results", "status"];
   
   clear() {
     this.resultsTarget.innerHTML = "";
@@ -15,6 +15,16 @@ export default class extends Controller {
     if (e.target.value === "") {
       this.clear();
     }
+  }
+
+  enable() {
+    this.submitButtonTarget.classList.remove("button--disabled");
+    this.submitButtonTarget.classList.add("button--primary");
+  }
+
+  disable() {
+    this.submitButtonTarget.classList.remove("button--primary");
+    this.submitButtonTarget.classList.add("button--disabled");
   }
 
   showStatus() {
