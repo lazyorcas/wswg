b43f3bbcad1ce68db401854018dc5f7e39e1ae06
lazyorcas <thelazyorcas@gmail.com>
2025-06-16 15:35:09 +0800

    fix

--- app/jobs/create_search_queries_job.rb
+++ app/jobs/create_search_queries_job.rb
@@ -9,6 +9,8 @@ class CreateSearchQueriesJob < ApplicationJob
       next if !skip_hour_check && city.time_zone.current_hour != HOUR_TO_QUERY
 
       EventCategory::SYMBOLS.each do |event_category_symbol|
+        next if event_category_symbol == :events
+
         SearchQuery.create(
           city: city,
           query: EventCategory.new(event_category_symbol).query
