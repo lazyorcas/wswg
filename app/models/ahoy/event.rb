cbff70bda28e1806b0fd485a1a5b265f7a0c404f
lazyorcas <thelazyorcas@gmail.com>
2025-06-18 14:08:07 +0800

    fix

--- app/models/ahoy/event.rb
+++ app/models/ahoy/event.rb
@@ -2,7 +2,7 @@ class Ahoy::Event < ApplicationRecord
   include Ahoy::QueryMethods
 
   scope :legitimate, -> { joins(:visit).where("ahoy_visits.referrer IS NULL OR ahoy_visits.landing_page IS NULL OR ahoy_visits.referrer != ahoy_visits.landing_page") }
-  scope :non_admin, -> { where.not(user_id: 1) }
+  scope :non_admin, -> { where("user_id IS NULL OR user_id != 1") }
   scope :visitors, -> { where.missing(:user).legitimate }
 
   self.table_name = "ahoy_events"
