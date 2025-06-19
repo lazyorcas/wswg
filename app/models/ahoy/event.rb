4e369b7baa66d7062dd3a29c1ee242c3f3a5b1c9
lazyorcas <thelazyorcas@gmail.com>
2025-06-19 22:50:21 +0800

    fix

--- app/models/ahoy/event.rb
+++ app/models/ahoy/event.rb
@@ -9,16 +9,4 @@ class Ahoy::Event < ApplicationRecord
 
   belongs_to :visit
   belongs_to :user, optional: true
-
-  # TODO: use a job instead
-  # after_create_commit :sync_visit_duration
-
-  # private
-
-  # def sync_visit_duration
-  #   duration_sync = Ahoy::Visit::DurationSync.new(visit: visit)
-  #   duration_sync.call
-  # rescue => e
-  #   Sentry.capture_exception(e)
-  # end
 end
