adad10a5e28b4d4808c8e0f3fe9e8c7977053de7
lazyorcas <thelazyorcas@gmail.com>
2025-06-21 16:36:14 +0800

    fix

--- app/controllers/map/bookmarks/events_controller.rb
+++ app/controllers/map/bookmarks/events_controller.rb
@@ -1,9 +1,14 @@
 class Map::Bookmarks::EventsController < ApplicationController
+  include CityDetection
+
   before_action :require_user!
 
   def index
     load_events
     load_time_zone
+    if @time_zone.nil?
+      respond_to_city_not_found and return
+    end
     filter_out_past_events
     order_events
     @events = @events.includes(:location, :source, :city)
@@ -16,7 +21,10 @@ class Map::Bookmarks::EventsController < ApplicationController
   end
 
   def load_time_zone
-    @time_zone = Current.user.city.time_zone
+    @time_zone = begin
+      city = get_city_from_visit || get_city_from_current_person
+      city.time_zone
+    end
   end
 
   def filter_out_past_events
