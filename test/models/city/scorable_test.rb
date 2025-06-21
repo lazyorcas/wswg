dc9680612f15a077daaed9f8c54dea409e894647
lazyorcas <thelazyorcas@gmail.com>
2025-06-21 22:30:23 +0800

    fix test

--- test/models/city/scorable_test.rb
+++ test/models/city/scorable_test.rb
@@ -1,15 +1,11 @@
 require "test_helper"
 
 class City::ScorableTest < ActiveSupport::TestCase
-  test "London should have score = 0" do
-    assert_equal 0, cities(:london).current_score
+  test "Singapore should have score = 0.01" do
+    assert_equal 0.01, cities(:singapore).current_score
   end
 
-  test "Singapore should have score = 0.1" do
-    assert_equal 0.1, cities(:singapore).current_score
-  end
-
-  test "Munich should have score = 1" do
-    assert_equal 1, cities(:munich).current_score
+  test "Munich should have score = 0" do
+    assert_equal 0, cities(:munich).current_score
   end
 end
