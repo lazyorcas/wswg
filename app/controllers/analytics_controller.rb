4f0661935388e9fe82bd8370d5c17e4a4b65924a
lazyorcas <thelazyorcas@gmail.com>
2025-06-22 00:52:14 +0800

    fix

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -127,13 +127,16 @@ class AnalyticsController < AdminController
           series[:data] << [ "#{day}#{ordinal_suffix(day)} #{@time_interval}", visit_counts.count { |count| count >= day } ]
         end
         visitor_retention_total = series[:data].first[1]
+        day[0] = "#{day[0]} (#{visitor_retention_total})"
         series[:data].each_with_index do |day, index|
           day[1] = (day[1].to_f / visitor_retention_total * 100).round(2)
         end
       end
       @visitor_retention << series
     end
-    @visitor_retention = @visitor_retention.sort_by { |series| series[:name] }
+    @visitor_retention = @visitor_retention
+      .sort_by { |series| series[:name] }
+      .reject { |series| series[:data].empty? }
 
     @events_created_by_source = Event
       .joins(:city_source)
