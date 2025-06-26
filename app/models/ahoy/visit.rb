325e9a56298edd4940234ffdc5f17c000cea105e
lazyorcas <thelazyorcas@gmail.com>
2025-06-26 13:58:55 +0800

    fix

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -3,7 +3,7 @@ class Ahoy::Visit < ApplicationRecord
 
   self.table_name = "ahoy_visits"
 
-  scope :legitimate, -> { Rails.env.production? ? where("user_id IS NOT NULL OR duration > 0 OR referrer_host != ?", ENV["HOST_NAME"]) : all }
+  scope :legitimate, -> { Rails.env.production? ? where("(user_id IS NOT NULL OR duration > 0) AND referrer_host != ?", ENV["HOST_NAME"]) : all }
   scope :non_admin, -> { where("user_id IS NULL OR user_id != 1") }
 
   # belongs_to :city, primary_key: "name", foreign_key: "city"
