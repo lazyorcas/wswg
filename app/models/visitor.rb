0a67c3efc2352d48f4d459a7be1dacb2e5ba3716
lazyorcas <thelazyorcas@gmail.com>
2025-07-02 12:27:41 +0800

    fix

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -10,4 +10,12 @@ class Visitor < ApplicationRecord
   has_many :field_test_memberships, class_name: "FieldTest::Membership", as: :participant, dependent: :destroy
 
   validates :visitor_token, presence: true, uniqueness: true
+
+  after_create_commit :capture_no_visits, if: -> { visits.empty? }
+
+  private
+
+  def capture_no_visits
+    Sentry.capture_message("Visitor has no visits", level: :warning, extra: { id: id })
+  end
 end
