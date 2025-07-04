7e7575ae8477a550dd1de0464845d54b18be0751
lazyorcas <thelazyorcas@gmail.com>
2025-07-04 17:33:54 +0800

    fix

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -52,7 +52,7 @@ class AnalyticsController < AdminController
       .group(:id)
       .order(id: :desc)
       .includes(:user, :events)
-      .slide(100)
+      .slice(100)
       .map do |visit|
         {
           started_at: visit.started_at,
