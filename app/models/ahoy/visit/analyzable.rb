23a2d5005596dff58db7ad45751591460dcfaaf7
lazyorcas <thelazyorcas@gmail.com>
2025-07-05 22:37:18 +0800

    analytics

--- app/models/ahoy/visit/analyzable.rb
+++ app/models/ahoy/visit/analyzable.rb
@@ -6,12 +6,16 @@ module Ahoy::Visit::Analyzable
   end
 
   def should_be_analyzed?
-    (user_id.present? || !bounced?) && user_id != 1
+    (user_id.present? || !bounced?) && user_id != 1 && valid_referrer_host?
   end
 
   private
 
   def bounced?
-    duration.nil? || duration == 0
+    duration.nil? || duration == 0 if started_at >= "2025-06-19T00:00:00Z"
+  end
+
+  def valid_referrer_host?
+    referrer_host.nil? || referrer_host != ENV["HOST_NAME"]
   end
 end
