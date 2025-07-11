98512de834ace958a661b36ee33b49bd8bdb4bc5
lazyorcas <thelazyorcas@gmail.com>
2025-07-11 13:16:48 +0800

    fix

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -1,4 +1,4 @@
-module CityHelper
+module CacheHelper
   def build_event_cache_key(event)
     key_array = [ dom_id(event), event.updated_at.to_s, @city.time_zone.current_date.to_s ]
     key_array << "bot" if browser.bot?
