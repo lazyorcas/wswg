24ff520fc583d0e5d3368385a973d98ef677c769
lazyorcas <thelazyorcas@gmail.com>
2025-07-14 12:06:46 +0800

    wip

--- app/jobs/create_search_queries_job.rb
+++ app/jobs/create_search_queries_job.rb
@@ -4,12 +4,12 @@ class CreateSearchQueriesJob < ApplicationJob
   queue_as :default
   queue_with_priority 0
 
-  def perform(skip_hour_check: false)
+  def perform
     event_category_symbols = EventCategory::SYMBOLS - [ :events ]
     event_categories = event_category_symbols.map { |symbol| EventCategory.new(symbol) }
 
     City.enabled.find_each do |city|
-      next if !skip_hour_check && city.time_zone.current_hour != HOUR_TO_QUERY
+      next if city.time_zone.current_hour != HOUR_TO_QUERY
 
       event_categories.each do |event_category|
         SearchQuery.create(
