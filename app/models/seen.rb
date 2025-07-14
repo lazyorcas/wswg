bcdad5c138918fddba557b0a40a27ed634f18b22
lazyorcas <thelazyorcas@gmail.com>
2025-07-14 23:03:26 +0800

    wip

--- app/models/seen.rb
+++ app/models/seen.rb
@@ -1,6 +1,6 @@
 class Seen < ApplicationRecord
   belongs_to :seenable, polymorphic: true, optional: true
-  belongs_to :event, class_name: "::Event", touch: true
+  belongs_to :event, class_name: "::Event"
 
   validates :event_id, uniqueness: { scope: [ :seenable_type, :seenable_id ] }, if: -> { seenable.present? }
 
