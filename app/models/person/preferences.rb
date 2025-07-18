a7d4b253b031d3bafbf87f386cd55635dbd0cc3a
lazyorcas <thelazyorcas@gmail.com>
2025-07-18 23:25:03 +0800

    fix

--- app/models/person/preferences.rb
+++ app/models/person/preferences.rb
@@ -1,12 +1,12 @@
 module Person::Preferences
   extend ActiveSupport::Concern
 
-  DEFAULT_SORT_BY = "popularity".freeze
+  # DEFAULT_SORT_BY = "popularity".freeze
 
   included do
     has_settings do |s|
       s.key :personalization
-      s.key :preferences, defaults: { sort_by: DEFAULT_SORT_BY }
+      s.key :preferences
     end
   end
 end
