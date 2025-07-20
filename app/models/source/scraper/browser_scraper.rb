65b2acf85d568e2748ec0b6a46f308dc6561e874
lazyorcas <thelazyorcas@gmail.com>
2025-07-20 22:08:59 +0800

    fix

--- app/models/source/scraper/browser_scraper.rb
+++ app/models/source/scraper/browser_scraper.rb
@@ -27,7 +27,6 @@ class Source::Scraper::BrowserScraper < Source::Scraper::BaseScraper
       end
     rescue => e
       begin
-        browser.reset
         browser.quit
       rescue
       end
@@ -39,7 +38,6 @@ class Source::Scraper::BrowserScraper < Source::Scraper::BaseScraper
       end
     ensure
       begin
-        browser.reset
         browser.quit
       rescue
       end
