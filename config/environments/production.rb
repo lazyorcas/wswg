67ffd4bbcab06277b93849fec0bdd1d11dbb4f63
lazyorcas <thelazyorcas@gmail.com>
2025-07-22 14:50:26 +0800

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,10 +35,14 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger = Logtail::Logger.create_default_logger(
-    ENV["LOGTAIL_TOKEN"],
-    ingesting_host: ENV["LOGTAIL_INGESTING_HOST"]
-  )
+  config.logger = if ENV["LOGTAIL_TOKEN"].present? && ENV["LOGTAIL_INGESTING_HOST"].present?
+    Logtail::Logger.create_default_logger(
+      ENV["LOGTAIL_TOKEN"],
+      ingesting_host: ENV["LOGTAIL_INGESTING_HOST"]
+    )
+  else
+    ActiveSupport::TaggedLogging.logger(STDOUT)
+  end
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
