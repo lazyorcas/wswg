7fdb9d492816b51b7b96a69736cbfb82523a5b63
lazyorcas <thelazyorcas@gmail.com>
2025-07-23 13:41:25 +0800

    handle meetup organizer urls

--- app/models/concerns/event/handles_sources.rb
+++ app/models/concerns/event/handles_sources.rb
@@ -2,4 +2,5 @@ module Event::HandlesSources
   extend ActiveSupport::Concern
 
   include Luma
+  include Meetup
 end
---
+++ app/models/concerns/event/handles_sources/meetup.rb
@@ -0,0 +1,26 @@
+module Event::HandlesSources::Meetup
+  extend ActiveSupport::Concern
+
+  included do
+    before_validation :modify_meetup_organizer_url, if: -> { from_meetup? && should_modify_meetup_organizer_url? }
+  end
+
+  def from_meetup?
+    source.name == "Meetup"
+  end
+
+  def should_modify_meetup_organizer_url?
+    url.include?("attendees")
+  end
+
+  def modify_meetup_organizer_url
+    return unless should_modify_meetup_organizer_url?
+
+    self.organizer_url = Url.build_url(url, "/#{group_slug}")
+  end
+
+  def meetup_group_slug
+    uri = Url.parse(url)
+    uri.path.split("/").second
+  end
+end
