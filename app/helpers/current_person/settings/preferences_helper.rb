fd9237e6831be8b6ecf43dc61e34b2b974de28bc
lazyorcas <thelazyorcas@gmail.com>
2025-07-27 11:22:01 +0700

    fix

--- app/helpers/current_person/settings/preferences_helper.rb
+++ app/helpers/current_person/settings/preferences_helper.rb
@@ -43,7 +43,11 @@ module CurrentPerson::Settings::PreferencesHelper
   # Hide impression events
   def hide_impression_events_value
     Current.person.settings(:preferences).hide_impression_events ||
-      field_test_variant(:hide_impression_events)
+      (
+        Current.person.returning? ?
+          Person::Preferences::DEFAULT_HIDE_IMPRESSION_EVENTS :
+          field_test_variant(:hide_impression_events)
+      )
   end
 
   def hide_impression_events_values
--- app/models/person/preferences.rb
+++ app/models/person/preferences.rb
@@ -2,6 +2,7 @@ module Person::Preferences
   extend ActiveSupport::Concern
 
   DEFAULT_SORT_BY = "time".freeze
+  DEFAULT_HIDE_IMPRESSION_EVENTS = "show".freeze
 
   included do
     has_settings do |s|
