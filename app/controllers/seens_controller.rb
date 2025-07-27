d81d77aae6bf25cf060fef6bde260e28dd8ff018
lazyorcas <thelazyorcas@gmail.com>
2025-07-27 11:11:59 +0700

    fix

--- app/controllers/seens_controller.rb
+++ app/controllers/seens_controller.rb
@@ -6,7 +6,7 @@ class SeensController < ApplicationController
     ahoy.track "Viewed event", event_id: seen_params[:event_id], source: params[:source], sort_by: sort_by
 
     convert_field_test(:sort_by_interests)
-    convert_field_test(:hide_impression_events)
+    convert_field_test(:hide_impression_events) if Current.person.returning?
 
     find_or_create_seen!
     head(:ok)
