6e5652d5554762dac300f01387ce4f429c6bfe65
lazyorcas <thelazyorcas@gmail.com>
2025-08-26 18:16:23 +0800

    fix

--- app/jobs/events/update_impression_counts_job.rb
+++ app/jobs/events/update_impression_counts_job.rb
@@ -2,12 +2,11 @@ class Events::UpdateImpressionCountsJob < ApplicationJob
   queue_as :default
   queue_with_priority 2
 
-  def perform(time_ago: 1.hour)
+  def perform
     Ahoy::Event
       .where(name: "Impression")
       .where("user_id != 1 OR user_id IS NULL")
       .group("properties->>'event_id'")
-      .where(time: time_ago.ago..)
       .count
       .each do |event_id, count|
         Event.find(event_id).update!(impression_count: count)
