cee5882381d62d2ba6e77e2c5c550ad984040e19
lazyorcas <thelazyorcas@gmail.com>
2025-08-26 11:59:05 +0800

    fix

--- app/schemas/event_attendees_count_schema.rb
+++ app/schemas/event_attendees_count_schema.rb
@@ -1,5 +1,5 @@
 class EventAttendeesCountSchema < RubyLLM::Schema
-  number :attendees_count, description: "Number of attendees. Use null if unknown" do
+  any_of :attendees_count, description: "Number of attendees. Use null if unknown" do
     number
     null
   end
